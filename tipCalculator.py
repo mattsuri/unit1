65517a5b7306a9305e06360bec95df6e53ccfb21
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-01-17 08:39:29 -0500

    Create tipCalculator.py

---
+++ tipCalculator.py
@@ -0,0 +1,7 @@
+#Matthew Suriawinata
+#1/17/18
+#tipCalculator.py - tips
+
+bill = int(input("How much was your meal? ")
+print("Your should tip that waiter", bill * 0.15, "for a final total of", bill * 1.15")
+
