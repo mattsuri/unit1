9f70631a41e17b9e314dd0b3d90e5ddd0a60b020
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-01-17 08:36:35 -0500

    Updated from Brython Server: 1/17/2018 8:36:35 AM

--- Age.py
+++ Age.py
@@ -1,3 +1,17 @@
 #Matthew Suriawinata
 #1/17/18
 #Age.py - age
+
+
+
+name = input('What is your name? ')
+print('Hello', name)
+first_name, last_name = name.split()
+
+age = int(input('What is your age?'))
+print ('you are' , age, 'years old')
+
+print("Your first name has", len(first_name), "letters")
+print("Your last name has", len(last_name), "letter")
+print("Next year you will be ", age + 1, "years old")
+
