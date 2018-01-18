07e50fae85cc865e12f31f5ad9b196c1a27717e0
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-01-18 13:39:06 -0500

    Updated from Brython Server: 1/18/2018 1:39:09 PM

--- slope.py
+++ slope.py
@@ -1,1 +1,18 @@
+#Matthew Suriawinata    
+#1/18/18
+#slope.py - slopes
 
+x1 = int(input("x1 = "))
+
+y1 = int(input("y1 = "))
+
+x2 = int(input("x2 ="))
+
+y2 = int(input("y2 ="))
+
+slope = (y1 - y2 )/(x1 - x2)
+
+yintercept = y1 - (slope * x1) 
+
+print("Your slope is ", slope)
+print("Your equation is y = ", slope, "x +", yintercept)
