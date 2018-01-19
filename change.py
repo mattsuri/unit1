146156feb983f2e319780fb0fb5bc1b8c80b44c5
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-01-19 08:38:42 -0500

    Updated from Brython Server: 1/19/2018 8:38:43 AM

--- change.py
+++ change.py
@@ -2,3 +2,14 @@
 #1/19/18
 #change.py - converts your change into coins
 
+initialcents = int(input("Number of cents: "))
+
+quarters = int(initialcents/25)
+dimes = int((initialcents - (25 *quarters))/10)
+nickles = int(initialcents + (-25 * quarters) + (-10 * dimes)/ 5)
+pennies = int(initialcents  + (-25 * quarters) + (-10 * dimes) + (-5 * nickles)/ 1)
+
+print("Quarters: ", quarters)
+print("Dimes: ", dimes)
+print("Nickles: ", nickles)
+print("Pennies: ", pennies)
