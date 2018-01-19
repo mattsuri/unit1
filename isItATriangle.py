40fa32cf051bbf0b1d966e85fdcfc495abea349b
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-01-19 08:51:34 -0500

    Updated from Brython Server: 1/19/2018 8:51:38 AM

--- isItATriangle.py
+++ isItATriangle.py
@@ -1,1 +1,10 @@
+#Matthew Suriawinata
+#1/19/18
+#isItATriangle.py - Whether a the shape is a triangle
+
+sideA = float(input("Enter Side A: "))
+sideB = float(input("Enter Side B: "))
+sideC = float(input("Enter Side C: "))
+
+print( 
 
