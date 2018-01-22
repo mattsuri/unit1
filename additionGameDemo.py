dee3844e06e8d73457c20f819ca2bb41e0598e1f
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-01-22 08:11:27 -0500

    Updated from Brython Server: 1/22/2018 8:11:28 AM

--- additionGameDemo.py
+++ additionGameDemo.py
@@ -1,3 +1,15 @@
 #Matthew Suriawinata
 #1/22/18
-#additionGameDemo.py - random numbers
+#additionGameDemo.py - how to use random numbers
+
+from random import randint
+
+num1 = randint(-10, 10)
+num2 = randint(-10, 10)
+
+answer = num1 + num2
+
+print(num1, "+", num2)
+
+inputanswer = int(input("= ")
+print(inputanswer == answer)
