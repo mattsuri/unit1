87016ca8ece4386f45cee7d0fed2886e75d1c126
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-01-22 08:41:28 -0500

    Updated from Brython Server: 1/22/2018 8:41:29 AM

--- isItATriangle.py
+++ isItATriangle.py
@@ -6,5 +6,14 @@ sideA = float(input("Enter Side A: "))
 sideB = float(input("Enter Side B: "))
 sideC = float(input("Enter Side C: "))
 
-print( 
+hyp = max(sideA, sideB, sideC)
+leg1 = min(sideA, sideB, sideC)
+
+
+perimeter = sideA + sideB + sideC
+leg2 = perimeter - hpy - leg1
+
+print(leg1 + leg2 > hpy)
+
+
 
