4843f5ccd15e9d428c67d80dd7bafebe467a927b
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-01-24 08:33:09 -0500

    Updated from Brython Server: 1/24/2018 8:33:12 AM

--- binary.py
+++ binary.py
@@ -17,4 +17,4 @@ decimalNumber = ((first*2**0) + (second*2**1) + (thrid*2**2) + (fourth*2**3) +
                 (fifth*2**4) + (sixth*2**5) + (seventh*2**6) + (eighth*2**7))
 
 
-print(decinmalNumber)
+print(decimalNumber)
