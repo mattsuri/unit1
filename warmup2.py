55b81793e0bb8efb9072546e219d8938edb80b4b
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-01-25 08:14:58 -0500

    Update warmup2.py

--- warmup2.py
+++ warmup2.py
@@ -1,1 +1,10 @@
+#Matthew Suriawinata
+#1/25/18
+#warmup2.py - prints name and lucky number
+from random import randint
+
+name = input("What is your name? ")
+
+
+print("Your name is ", name, ". Your lucky number is", randint(0, 10))
 
