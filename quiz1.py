a7b0bed1a3edf34d05c28f8e77b0820a21c31013
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-01-26 08:17:42 -0500

    Updated from Brython Server: 1/26/2018 8:17:44 AM

--- quiz1.py
+++ quiz1.py
@@ -1,1 +1,13 @@
+#Matthew Suriawinata    
+#1/26/18
+#quiz1.py 
 
+print("Hello")
+name = input("What is your name? ")
+
+favNum1 = int(input("What is your favorite number? "))
+print(name * favNum1)
+
+favNum2 = int(input("What is your second favorite number? "))
+
+print("Your lucky number is: ", favNum1**favNum2)
